9eaecba59e3bef9b3d45a5d93f1bc73e76c8530a
dylanhuntoon <drhuntoon@gmail.com>
2022-09-14 15:26:43 -0400

    Added Keg object for multi-system support

--- keg-app/keg_app/lib/main.dart
+++ keg-app/keg_app/lib/main.dart
@@ -4,6 +4,21 @@ void main() {
   runApp(const MyApp());
 }
 
+class Keg {
+  int temp = 0;
+  int vol = 0;
+  bool connection = false;
+  String ID;
+  String name = "unnamed";
+
+  Keg(this.ID);
+
+  @override
+  String toString() {
+    return "Keg [ID=$ID, name=$name, temp=$temp, vol=$vol]";
+  }
+}
+
 class KegList extends StatefulWidget {
   const KegList({super.key});
 
@@ -12,6 +27,12 @@ class KegList extends StatefulWidget {
 }
 
 class _KegListState extends State<KegList> {
+  List<Keg> kegs = [
+    Keg(123456789.toString()),
+    Keg(123456790.toString()),
+    Keg(123456791.toString())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +40,20 @@ class _KegListState extends State<KegList> {
             AppBar(title: const Text('Kool Keg - Mobile Monitoring System')),
         body: ListView.builder(
           padding: const EdgeInsets.all(8.0),
+          itemCount: kegs.length,
           itemBuilder: (context, i) {
-            if (i.isOdd) return const Divider();
-
-            final index = i ~/ 2;
-            return ListTile(
-              title: Text("Beverage Name for Keg $index"),
-              subtitle: const Text("ID: 123456789"),
-              trailing: const Icon(
-                Icons.check_circle,
-                color: Colors.green,
-                semanticLabel: 'Edit',
+            return Card(
+                child: ListTile(
+              title: Text("${kegs[i].name}"),
+              subtitle: Text("ID: ${kegs[i].ID}"),
+              trailing: Icon(
+                kegs[i].connection == true ? Icons.check_circle : Icons.error,
+                color:
+                    kegs[i].connection == true ? Colors.green : Colors.orange,
+                semanticLabel:
+                    kegs[i].connection == true ? "Connected" : "Disconnected",
               ),
-            );
+            ));
           },
         ));
   }
@@ -46,7 +68,7 @@ class MyApp extends StatelessWidget {
       title: "Keg App",
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
-              backgroundColor: Colors.blue, foregroundColor: Colors.white)),
+              backgroundColor: Colors.black, foregroundColor: Colors.white)),
       home: const KegList(),
     );
   }
