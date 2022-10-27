b5da96735578cb63f143958a9e698344ae64a57d
Dylan Huntoon <drhuntoon@gmail.com>
2022-10-27 12:57:20 -0400

    added comments to code for documentation

--- Keg_App/aws_backend/getLmabda.js
+++ Keg_App/aws_backend/getLmabda.js
@@ -3,8 +3,10 @@ AWS.config.update({region:"us-east-2"});
 
 exports.handler = async (event, context) => {
     
+    // create document client object
     const dc = new AWS.DynamoDB.DocumentClient({region:"us-east-2"});
-    
+   
+    // create request parameters from event values
     const params = {
         TableName : "kegs",
         KeyConditionExpression: "kegId = :kegId",
@@ -12,12 +14,14 @@ exports.handler = async (event, context) => {
             ":kegId" : event.kegId
         }
     };
-    
+   
+    // attempt to get the data from dynamodb
     try {
         var data = await dc.query(params).promise()
         console.log(data);
         return data.Items
     } catch (err) {
+        // if an error occurs, log the error and return
         console.log(err);
         return err;
     }
